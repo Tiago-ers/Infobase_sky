16f491b26b903468a6446d19300ca31c95692302
Tiago-ers <trsilva284@gmail.com>
2021-04-23 00:14:53 -0300

    conect mongodb atlas

--- database/index.js
+++ database/index.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 
+//'mongodb://localhost:27017/sky'
+
 mongoose.Promise = global.Promise;
 mongoose
-  .connect('mongodb://localhost:27017/sky', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
+  .connect(
+    'mongodb+srv://tiago:123@cluster0.i4x3i.mongodb.net/sky?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }
+  )
   .then(() => {
     console.log('Mongo conectado');
   })
